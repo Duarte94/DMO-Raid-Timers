10404dd5c7db22f34d08620003cc00c58c3eadb0
Duarte94 <duarte.cdf94@gmail.com>
2024-06-11 23:59:43 -0300

    added vercel and sqlite

--- script.js
+++ script.js
@@ -1,45 +1,34 @@
-// URL del backend en Vercel
-const backendURL = 'https://dmott-cbo0uxprd-devas-projects-20feeb98.vercel.app';
+const backendURL = 'https://dmott-cbo0uxprd-devas-projects-20feeb98.vercel.app/';
 
-// Array para almacenar los intervalos de los temporizadores
 let timers = [null, null, null];
 
-// Función para iniciar el temporizador
 async function startTimer(index) {
-  // Obtener los valores de horas y minutos del formulario
   let hours = parseInt(document.getElementById('hours' + index).value);
   let minutes = parseInt(document.getElementById('minutes' + index).value);
-
-  // Calcular la duración en milisegundos
   let duration = (hours * 60 * 60 + minutes * 60) * 1000;
 
   try {
-    // Hacer una solicitud POST al backend para iniciar el temporizador
     const response = await fetch(`${backendURL}/start-timer`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ index: index, duration: duration })
+      body: JSON.stringify({ index, duration })
     });
 
     if (!response.ok) {
       throw new Error('Failed to start timer');
     }
 
-    // Obtener la respuesta JSON del backend
     const data = await response.json();
     console.log('Timer started:', data);
 
-    // Guardar el tiempo de finalización del temporizador
     const endTime = data.endTime;
 
-    // Si ya hay un temporizador activo, detenerlo
     if (timers[index - 1]) {
       clearInterval(timers[index - 1]);
     }
 
-    // Iniciar un intervalo para actualizar el temporizador en la interfaz de usuario
     timers[index - 1] = setInterval(function () {
       const now = Date.now();
       const distance = endTime - now;
@@ -65,27 +54,23 @@ async function startTimer(index) {
   }
 }
 
-// Función para detener el temporizador
 async function resetTimer(index) {
   try {
-    // Hacer una solicitud POST al backend para detener el temporizador
     const response = await fetch(`${backendURL}/stop-timer`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ index: index })
+      body: JSON.stringify({ index })
     });
 
     if (!response.ok) {
       throw new Error('Failed to stop timer');
     }
 
-    // Obtener la respuesta JSON del backend
     const data = await response.json();
     console.log('Timer stopped:', data);
 
-    // Limpiar el intervalo y restablecer el temporizador en la interfaz de usuario
     clearInterval(timers[index - 1]);
     document.getElementById('display' + index).innerText = "00:00:00";
 
